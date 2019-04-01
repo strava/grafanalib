b607ba9262480c1aecbb65acff578462642fad40
Matt Davis <mdavis@strava.com>
2019-04-01 14:08:34 -0700

    Add target and rename datasource

--- grafanalib/core.py
+++ grafanalib/core.py
@@ -4635,11 +4635,11 @@ class BarChart(Panel):
 @attr.s
 class Annotation(object):
     """
-    Annotation create a new dashboard annotation.  Annotations can be defined to query a
+    Annotation creates a new dashboard annotation.  Annotations can be defined to query a
     datasource and overlay information related to key events, such as container exits or deploys.
 
         :param default: the default value for the variable
-        :param dataSource: where to fetch the values for the variable from
+        :param datasource: where to fetch the values for the variable from
         :param label: the variable's human label
         :param name: the variable's name
         :param expr: the query users to fetch the valid values of the variable
@@ -4660,10 +4660,11 @@ class Annotation(object):
             SHOW (default), HIDE_LABEL, HIDE_VARIABLE
     """
     name = attr.ib()
-    expr = attr.ib()
+    expr = attr.ib(default=None)
+    target = attr.ib(default=None)
     _current = attr.ib(init=False, default=attr.Factory(dict))
     default = attr.ib(default=None)
-    dataSource = attr.ib(default=None)
+    datasource = attr.ib(default=None)
     label = attr.ib(default=None)
     allValue = attr.ib(default=None)
     hide = attr.ib(default=SHOW)
@@ -4723,7 +4724,7 @@ class Annotation(object):
         return {
             'allValue': self.allValue,
             'current': self._current,
-            'datasource': self.dataSource,
+            'datasource': self.datasource,
             'enable': self.enable,
             'expr': self.expr,
             'hide': self.hide,
@@ -4737,6 +4738,7 @@ class Annotation(object):
             'regex': self.regex,
             'sort': 1,
             'step': self.step,
+            'target': self.target,
             'textFormat': self.textFormat,
             'type': self.type,
             'useTags': self.useTags,
